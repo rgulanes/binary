f22345d88b85eb7fcff2056fb7692be018984400
Rhonnel <rhonneljickaincordova94@gmail.com>
2017-01-12 22:50:37 +0800

    update on sql

--- database/stored procedures/get_MembersAssigned.sql
+++ database/stored procedures/get_MembersAssigned.sql
@@ -10,7 +10,7 @@ BEGIN
 				FROM users u
 					LEFT JOIN position p ON p.user_id = u.user_id
 					LEFT JOIN users up ON up.user_id = p.upline
-				WHERE p.upline = userId AND p.position_left =  1;
+				WHERE p.upline = userId AND p.position_left >  0;
 	ELSE
 			SELECT u.user_id, CONCAT(u.first_name, ' ', u.last_name) AS full_name, u.user_name,
 				p.position_right,p.position_left, CONCAT(up.first_name, ' ', up.last_name, ' (', up.user_name, ')') AS up_FName,
@@ -18,7 +18,7 @@ BEGIN
 				FROM users u
 					LEFT JOIN position p ON p.user_id = u.user_id
 					LEFT JOIN users up ON up.user_id = p.upline
-				WHERE p.upline = userId AND p.position_right =  1;
+				WHERE p.upline = userId AND p.position_right > 0;
 	END IF;
 END$$
 DELIMITER ;
--- database/stored procedures/get_downLines.sql
+++ database/stored procedures/get_downLines.sql
@@ -4,8 +4,8 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `get_downLines`(IN userId INT)
 BEGIN
 	SELECT u.user_id, CONCAT(u.first_name, ' ', u.last_name) AS full_name, u.user_name,
 	(CASE
-		WHEN p.position_right = 1 THEN 'R'
-		WHEN p.position_left = 1 THEN 'L'
+		WHEN p.position_right > 0 THEN 'R'
+		WHEN p.position_left > 0 THEN 'L'
 		ELSE ''
 	END) AS isPosition,
 	CONCAT(up.first_name, ' ', up.last_name, ' (', up.user_name, ')') AS up_FName,
