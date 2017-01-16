5f24160d39c36af4f370014dc6412a49a76a4fc6
Ronald Jay Gulanes <rgulanes@users.noreply.github.com>
2017-01-17 03:51:18 +0800

    Members - General
    * FIX - Update SP to correct showing of available downline.

--- database/stored procedures/get_AvailableDownline.sql
+++ database/stored procedures/get_AvailableDownline.sql
@@ -2,7 +2,7 @@ DROP PROCEDURE IF EXISTS get_AvailableDownline;
 DELIMITER $$
 
 # CALL get_AvailableDownline(15, 'R');
-CREATE DEFINER=`admindev_db`@`localhost` PROCEDURE `get_AvailableDownline`(IN userId INT, IN isPosition VARCHAR(2))
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_AvailableDownline`(IN userId INT, IN isPosition VARCHAR(2))
 BEGIN
       
       DROP TABLE IF EXISTS availableDownline;
@@ -13,17 +13,10 @@ BEGIN
 					  FROM position p
 						JOIN users u ON u.user_id = p.user_id
 						WHERE 
-							p.position_left <= 0
-						AND
-							EXISTS(SELECT pos.position_right 
-										  FROM   position pos
-										  WHERE  pos.position_right <> '' AND pos.position_right = u.user_id
-										 )
-							OR
-							EXISTS(SELECT pos.position_left 
+							p.position_left >= 0 AND EXISTS(SELECT pos.position_left 
 										  FROM   position pos
 										  WHERE  pos.position_left <> '' AND pos.position_left = u.user_id
-										 ));
+										 ) AND p.sponsor_by = userId);
       ELSE
 				  CREATE TEMPORARY TABLE IF NOT EXISTS availableDownline AS
                   (SELECT u.user_id as u_user_id,u.first_name as u_first_name,u.last_name as u_last_name,u.contact as u_contact,u.address as u_address,u.email as u_email,u.gender as u_gender,u.sponsor_by as u_sponsor,
@@ -31,17 +24,10 @@ BEGIN
 					  FROM position p
 						JOIN users u ON u.user_id = p.user_id
 					  WHERE 
-						p.position_right <= 0
-						AND
-						EXISTS(SELECT pos.position_right 
-									  FROM   position pos
-									  WHERE  pos.position_right <> '' AND pos.position_right = u.user_id
-									 )
-						OR
-						EXISTS(SELECT pos.position_left 
-									  FROM   position pos
-									  WHERE  pos.position_left <> '' AND pos.position_left = u.user_id
-									 ));
+						p.position_right >= 0 AND EXISTS(SELECT pos.position_right 
+										  FROM   position pos
+										  WHERE  pos.position_right <> '' AND pos.position_right = u.user_id
+										 ) AND p.sponsor_by = userId);
       END IF;
 
       SELECT * FROM availableDownline 
